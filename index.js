1944665e66f86e0e977bbaa4f0f1c149569c4832
Faruq-Hameed <faruqhameed1@gmail.com>
2022-11-09 16:09:45 +0100

    exported professionaly

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const {largeNumber,cut3} = require('./src/module1');
-const mathOperations = require('./src/module2')
+const sumAndConcat = require('./src/module2') 
 
 const numbers = [1,5,2,9,3,4,7]
 const letters = ['a','b','c','d','e']
 
 const results1 = largeNumber(numbers)
 const results2 = cut3(numbers)
-const results3 = mathOperations.sum(numbers);
-const results4= mathOperations.concat(numbers, letters)
+const results3 = sumAndConcat.sum(numbers);
+const results4= sumAndConcat.concat(numbers, letters)
 
 
 console.log(results1);//9
--- src/module2.js
+++ src/module2.js
@@ -1,21 +1,37 @@
 
-const sum = (arr) => {
-  //Assignment to constant variable//
-//   const total = 0;
-let total = 0;
-  arr.forEach((x) => {
-    total += x;
-  })
-  return total
+// const sum = (arr) => {
+//   //Assignment to constant variable//
+// //   const total = 0;
+// let total = 0;
+//   arr.forEach((x) => {
+//     total += x;
+//   })
+//   return total
+// }
+
+// // concatenate two arrays
+// const concat = (arr1, arr2) => {
+//   return [...arr1, ...arr2]
+// }
+
+// exports.sum = sum
+// exports.concat = concat
+
+
+
+module.exports ={
+   sum: (arr) => {
+      //Assignment to constant variable//
+    //   const total = 0;
+    let total = 0;
+      arr.forEach((x) => {
+        total += x;
+      })
+      return total
+    },
+    
+    // concatenate two arrays
+     concat: (arr1, arr2) => {
+      return [...arr1, ...arr2]
+    }
 }
-
-// concatenate two arrays
-const concat = (arr1, arr2) => {
-  return [...arr1, ...arr2]
-}
-
-exports.sum = sum
-exports.concat = concat
-
-
-
